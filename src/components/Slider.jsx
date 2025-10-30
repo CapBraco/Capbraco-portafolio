f1c4109377cf3005df4e60a48ba7e4fb7caa1b6b
BRACO <bracosmo@gmail.com>
2025-10-30 10:37:25 -0400

    video 4

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,12 @@
 // VideoCarousel.jsx
 import React, { useRef } from "react";
-
 const VIDEOS = [
-  { title: "Blender", src: "/videos/video1.mp4" },
-  { title: "Blender 1", src: "/videos/video2.mp4" },
-  { title: "Blender 2", src: "/videos/video3.mp4" },
-  { title: "Blender 3", src: "/videos/video4.mp4" },
-  { title: "Blender 5", src: "/videos/video5.mp4" },
-  { title: "Blender 6", src: "/videos/video6.mp4" },
+  { title: "Blender", src: "./videos/video1.mp4" },
+  { title: "Blender 1", src: "./videos/video2.mp4" },
+  { title: "Blender 2", src: "./videos/video3.mp4" },
+  { title: "Blender 3", src: "./videos/video4.mp4" },
+  { title: "Blender 5", src: "./videos/video5.mp4" },
+  { title: "Blender 6", src: "./videos/video6.mp4" },
 ];
 
 const Slider = () => {
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,13 @@
 const projects = [
-    {title: 'bryanpawkar.com', image: "/images/project1.png", id:'00'},
-    {title: 'Unasur – 3D Modeling & Rendering', image: "/images/project2.png", id:'01'},
-    {title: 'TLS Delivery & Courier', image: "/images/project3.png", id:'02'},
-    {title: 'React 4', image: "/images/project4.png", id:'03'},
-    {title: 'Lutweb.io', image: "/images/project5.png", id:'04'},
-    {title: "HCE10.com", image: "/images/project6.png", id:'05'},
-    {title: 'Unasur 3D Animation & Video Production', image: "/images/project7.png", id:'06'},
-    {title: 'mamutfastfood.com', image: "/images/project8.png", id:'07'},
-    {title: 'React 9', image: "/images/project9.png", id:'08'},
+  { title: 'bryanpawkar.com', image: "./images/project1.png", id:'00' },
+  { title: 'Unasur – 3D Modeling & Rendering', image: "./images/project2.png", id:'01' },
+  { title: 'TLS Delivery & Courier', image: "./images/project3.png", id:'02' },
+  { title: 'React 4', image: "./images/project4.png", id:'03' },
+  { title: 'Lutweb.io', image: "./images/project5.png", id:'04' },
+  { title: "HCE10.com", image: "./images/project6.png", id:'05' },
+  { title: 'Unasur 3D Animation & Video Production', image: "./images/project7.png", id:'06' },
+  { title: 'mamutfastfood.com', image: "./images/project8.png", id:'07' },
+  { title: 'React 9', image: "./images/project9.png", id:'08' },
 ]
 
 export default projects;
